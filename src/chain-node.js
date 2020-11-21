c2c85060a84071a336417463ebcd02e06ad0ccc8
Matheus Sproesser <matheus.sproesser@gmail.com>
2020-11-21 18:23:54 -0300

    first try p2p over internet

--- src/chain-node.js
+++ src/chain-node.js
@@ -13,7 +13,12 @@ export class ChainNode extends CustomNode {
     super({
       peerId,
       addresses: {
-        listen: ['/ip4/0.0.0.0/tcp/0']
+        listen: [
+          '/ip4/0.0.0.0/tcp/0/',
+          '/ip6/::/tcp/0',
+          '/ip4/0.0.0.0/tcp/0/ws',
+          '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
+        ]
       }
     })
     this.storage = new Storage()
--- src/libp2p-bundle.js
+++ src/libp2p-bundle.js
@@ -2,10 +2,12 @@
 
 import TCP from 'libp2p-tcp'
 import WS from 'libp2p-websockets'
+import WebrtcStar from 'libp2p-webrtc-star'
+import wrtc from 'wrtc'
 import mplex from 'libp2p-mplex'
-import SECIO from 'libp2p-secio'
 import { NOISE } from 'libp2p-noise'
-
+import Bootstrap from 'libp2p-bootstrap'
+import MDNS from 'libp2p-mdns'
 import DHT from 'libp2p-kad-dht'
 import GossipSub from 'libp2p-gossipsub'
 import defaultsDeep from '@nodeutils/defaults-deep'
@@ -16,14 +18,33 @@ export default class CustomNode extends libp2p {
   constructor (_options) {
     const defaults = {
       modules: {
-        transport: [ TCP, WS ],
+        transport: [ TCP, WS, WebrtcStar ],
         streamMuxer: [ mplex ],
-        connEncryption: [ NOISE, SECIO ],
+        connEncryption: [ NOISE ],
+        peerDiscovery: [ Bootstrap, MDNS ],
         dht: DHT,
         pubsub: GossipSub
+      },
+      config: {
+        transport : {
+          [WebrtcStar.prototype[Symbol.toStringTag]]: {
+            wrtc
+          }
+        },
+        peerDiscovery: {
+          bootstrap: {
+            list: [ '/dnsaddr/sjc-1.bootstrap.libp2p.io/tcp/4001/ipfs/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN' ]
+          }
+        },
+        dht: {
+          enabled: true,
+          randomWalk: {
+            enabled: true
+          }
+        }
       }
     }
-    
+    console.log(defaultsDeep(_options, defaults))
     super(defaultsDeep(_options, defaults))
   }
 }
